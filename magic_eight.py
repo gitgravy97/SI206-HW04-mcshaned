a67f637e19b31552c21ca692a3fc53468536b14f
McCoy Doherty <mcshaned@umich.edu>
2018-02-03 17:46:39 -0500

    Made random response into a function, set up the loop to ask until user enters keyword: quit

--- magic_eight.py
+++ magic_eight.py
@@ -4,6 +4,16 @@ def ask_question():
 	question = input("What is your question? ")
 	return(question)
 
+def check_question(question_in):
+	if(question_in[-1] != "?"):
+		print("I'm sorry, I can only answer questions.")
+		return False
+	else:
+		return True
+
+def random_response():
+	return(random.choice(ans))
+
 ans = ['It is certain', 'It is decidedly so', 'Without a doubt',
 		'Yes definitely', 'You may rely on it', 'As I see it, yes',
 		'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
@@ -11,5 +21,15 @@ ans = ['It is certain', 'It is decidedly so', 'Without a doubt',
 		'Cannot predict now', 'Concentrate and ask again', "Don't count on it",
 		'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 
-ask_question()
-print (random.choice(ans))
+#ask_question()
+#print (random.choice(ans))
+
+keep_going = True
+while(keep_going):
+	user_question = ask_question()
+	
+	if(user_question == "quit"):
+		keep_going = False
+
+	if(check_question(user_question) == True):
+		print(random_response())
